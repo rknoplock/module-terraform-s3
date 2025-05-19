4c9686835f6536eea2b0488eabc34b575e130c45
rknoplock <127435435+rknoplock@users.noreply.github.com>
2025-05-19 19:06:34 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.3.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
 resource "aws_s3_bucket" "this" {
   bucket        = var.bucket_name
   force_destroy = var.force_destroy
