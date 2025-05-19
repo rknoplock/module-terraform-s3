f47291b57ec4a056ab9361f96be82fdb86c76a5b
rknoplock <127435435+rknoplock@users.noreply.github.com>
2025-05-19 19:18:38 -0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -8,14 +8,14 @@ output "bucket_arn" {
   value       = aws_s3_bucket.this.arn
 }
 
-output "versioning_status" {
-  description = "Status do versionamento do bucket"
-  value       = aws_s3_bucket_versioning.this.versioning_configuration[0].status
-}
-
 output "encryption_algorithm" {
   description = "Algoritmo de criptografia configurado no bucket"
-  value       = aws_s3_bucket_server_side_encryption_configuration.this.rule[0].apply_server_side_encryption_by_default[0].sse_algorithm
+  value       = one(aws_s3_bucket_server_side_encryption_configuration.this.rule).apply_server_side_encryption_by_default[0].sse_algorithm
+}
+
+output "versioning_status" {
+  description = "Status do versionamento do bucket"
+  value       = one(aws_s3_bucket_versioning.this.versioning_configuration).status
 }
 
 output "public_access_blocked" {
