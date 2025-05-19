c9a82c9dae201c675a493d43c564bdd70f8c826c
rknoplock <127435435+rknoplock@users.noreply.github.com>
2025-05-19 19:36:59 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,3 +14,9 @@ variable "bucket_name" {
   description = "Nome do bucket S3"
   type        = string
 }
+
+variable "tags" {
+  description = "Tags do bucket"
+  type        = map(string)
+  default     = {}
+}
