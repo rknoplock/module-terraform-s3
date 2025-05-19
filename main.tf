08c70fc8c810080e3048422e38dfd8edd3b797b7
rknoplock <127435435+rknoplock@users.noreply.github.com>
2025-05-19 19:32:29 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,18 +13,10 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "this" {
-  bucket        = var.bucket_name
-  force_destroy = var.force_destroy
-  acl           = var.acl
+  bucket = var.bucket_name
+  acl    = var.acl
 
-  tags = var.tags
-}
-
-resource "aws_s3_bucket_public_access_block" "this" {
-  count                   = var.block_public_access ? 1 : 0
-  bucket                  = aws_s3_bucket.this.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  tags = {
+    Name = var.bucket_name
+  }
 }
