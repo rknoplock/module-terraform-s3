9b9c24a3ed5bbd8eea7b5d009401ecc3995bfac3
rknoplock <rafaelstp8@gmail.com>
2025-06-09 19:13:27 -0300

    removendo referencias

--- main.tf
+++ main.tf
@@ -1,18 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket = "rknoplock-remote-state"
-    key    = "s3-buckets/${var.bucket_name}.tfstate"
-    region = "us-east-1"
-  }
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0"
-    }
-  }
-}
-
 provider "aws" {
   region = "us-east-1"
 }
